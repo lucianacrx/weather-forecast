a912fe0b533aafdf60e65babd3bc76b9b958b94d
Ana De La Cruz <acruz@truenorth.pr>
2020-08-26 22:01:51 -0400

    add unit

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -7,8 +7,8 @@ const Day = (props) => {
             <h3>{props.date}</h3>
             <h3>{props.time}</h3>
             <i className={`wi ${props.icon} ${styles.weatherIcon}`}></i>
-            <h3>Max: {props.maxTemp}</h3>
-            <h3>Min: {props.minTemp}</h3>
+            <h3>Max: {props.maxTemp} °C</h3>
+            <h3>Min: {props.minTemp} °C</h3>
         </div>
     );
 }
